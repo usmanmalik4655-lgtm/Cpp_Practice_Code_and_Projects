28b28f08335b711a180b7b9bf5fc164b5be9a4c0
usmanmalik4655-lgtm <usmanmalik4655@gmail.com>
2025-10-14 07:03:38 -0700

    different concepts related to arithmetic operators

---
+++ using_compound.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+int main() {
+int x,y ;
+cin>>x>>y ;
+x+=y;
+cout<<"sum ="<<x<<endl;
+x-=y;
+cout<<"sub ="<<x<<endl;
+x*=y;
+cout<<"mul ="<<x<<endl;
+x/=y;
+cout<<"div ="<<x<<endl;
+x%=y;
+cout<<"mod ="<<x<<endl;
+
+return 0 ;
+
+}
