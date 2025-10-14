b8b3c94d618773f6bda7084025582e54ebaeeda2
usmanmalik4655-lgtm <usmanmalik4655@gmail.com>
2025-10-14 04:43:34 -0700

    different concepts related to arithmetic operators

---
+++ Arith_Opt.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std ;
+int main () {
+    float x,y ;
+    cout<<"Enter your value :"<<endl;
+    cin>>x>>y;
+    cout<<"sum ="<<x+y<<endl ;
+    cout<<"sub ="<<x-y<<endl;
+    cout<<"mul ="<<x*y<<endl;
+    cout<<"div ="<<x/y<<endl;
+    //cout<<"mod ="<<x%y<<endl;
+
+    return 0 ;
+
+}
---
+++ using_3_opt.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std ;
+int main () {
+    int r, x, y ;
+    cout<<"Enter values:"<<endl;
+    cin>>x>>y;
+    r=x+y;
+    cout<<"sum ="<< r << endl;
+    r=x-y;
+    cout<<"sub ="<<r<<endl;
+    r=x*y;
+    cout<<"mul ="<<r<<endl;
+    r=x/y;
+    cout<<"div ="<<r<<endl;
+    r=x%y;
+    cout<<"mod ="<<r<<endl;
+
+    return 0;
+
+}
