b543410834b7873eb1f600210df23816c0d454ee
usmanmalik4655-lgtm <usmanmalik4655@gmail.com>
2025-10-15 04:37:13 -0700

    celcius to faranhiet

---
+++ celcius_to_faranhiet.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+int main () {
+    int f;
+    cout<<"enter value :"<<endl;
+    cin>>f;
+
+    float c = (f-32)*5/9;
+    cout<<"c:"<<c;
+    
+    return 0 ;
+}
