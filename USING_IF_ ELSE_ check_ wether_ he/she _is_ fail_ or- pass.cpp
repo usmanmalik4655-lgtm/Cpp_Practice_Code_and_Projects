ad5e600eb926ad324d53eda6923184143f67b47f
usmanmalik4655-lgtm <usmanmalik4655@gmail.com>
2025-10-18 03:52:15 -0700

    using if else check fail or pass and programs for increment and decrement

---
+++ USING_IF_ ELSE_ check_ wether_ he/she _is_ fail_ or- pass.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int m;
+    cout<<"enter your marks :"<<endl;
+    cin>>m;
+    if(m>50) {
+        cout<<"pass :"<<endl;
+
+    }
+    else{
+     cout<<"fail :"<<endl;
+
+    }
+        return 0 ;
+}
