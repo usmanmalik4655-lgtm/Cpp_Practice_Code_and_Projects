da485b9db1a3db7c0292c22e8df4bab83ba7d420
usmanmalik4655-lgtm <usmanmalik4655@gmail.com>
2025-10-25 03:48:12 -0700

    using switch for two numbers and checking according to users operator

---
+++ switch _for_two_program_operator.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main() { 
+    int n1,n2;
+    char opt;
+    cout<<"enter 2 number :"<<endl;
+    cin>>n1>>n2;
+    cout<<"enter your operator:"<<endl;
+    cin>>opt;
+
+    switch(opt) {
+
+        case '+' :
+        cout<<"sum="<<n1+n2<<endl;
+        break;
+
+        case '-' :
+        cout<<"sub="<<n1-n2<<endl;
+        break;
+
+        case '*' :
+        cout<<"mul="<<n1*n2<<endl;
+        break;
+
+        case '/' :
+        cout<<"div="<<n1/n2<<endl;
+        break;
+
+        case '%' :
+        cout<<"mod="<<n1%n2<<endl;
+        break;
+
+        default:
+        cout<<"INVALID OPERATOR:"<<endl;
+    }
+       return 0;
+
+}
